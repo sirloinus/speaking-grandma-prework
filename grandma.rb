beb1f4d91e8885a9ca49767a7a1461dfc52cb230
Amalie S <amaliesmidth@gmail.com>
2018-08-17 15:38:08 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,22 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+
+# def speak_to_grandma(string)
+#   if string == string.upcase
+#     return 'NO, NOT SINCE 1938!'
+#   elsif string == 'I LOVE YOU GRANDMA!'
+#     return 'I LOVE YOU TOO PUMPKIN'
+#   else
+#     return 'HUH?! SPEAK UP, SONNY!'
+# end 
+
+def speak_to_grandma(string)
+  if string != string.upcase
+    return 'HUH?! SPEAK UP, SONNY!'
+  if string == 'I LOVE YOU GRANDMA!'
+    return 'I LOVE YOU TOO PUMPKIN'
+  if string == string.upcase
+    return 'NO, NOT SINCE 1938!'
+end
