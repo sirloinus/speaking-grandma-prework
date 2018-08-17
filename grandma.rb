14839ddddb9c69966260ee28a91d892928bfadaf
Amalie S <amaliesmidth@gmail.com>
2018-08-17 15:41:55 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,22 +13,22 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 
-def speak_to_grandma(string)
-  if string == string.upcase
-    return 'NO, NOT SINCE 1938!'
-  elsif string == 'I LOVE YOU GRANDMA!'
-    return 'I LOVE YOU TOO PUMPKIN'
-  else
-    return 'HUH?! SPEAK UP, SONNY!'
-``end 
-end 
-
 # def speak_to_grandma(string)
-#   if string != string.upcase
-#     return 'HUH?! SPEAK UP, SONNY!'
-#   if string == 'I LOVE YOU GRANDMA!'
-#     return 'I LOVE YOU TOO PUMPKIN'
 #   if string == string.upcase
 #     return 'NO, NOT SINCE 1938!'
-#   end
-# end
+#   elsif string == 'I LOVE YOU GRANDMA!' 
+#     return 'I LOVE YOU TOO PUMPKIN'
+#   else
+#     return 'HUH?! SPEAK UP, SONNY!'
+# ``end 
+# end 
+
+def speak_to_grandma(string)
+  if string != string.upcase
+    return 'HUH?! SPEAK UP, SONNY!'
+  if string == 'I LOVE YOU GRANDMA!'
+    return 'I LOVE YOU TOO PUMPKIN'
+  if string == string.upcase
+    return 'NO, NOT SINCE 1938!'
+  end
+end
