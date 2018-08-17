5c5ef402d39a1da39bf5313660080dd596bc7c17
Amalie S <amaliesmidth@gmail.com>
2018-08-17 15:44:10 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,16 +13,6 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 
-# def speak_to_grandma(string)
-#   if string == string.upcase
-#     return 'NO, NOT SINCE 1938!'
-#   elsif string == 'I LOVE YOU GRANDMA!' 
-#     return 'I LOVE YOU TOO PUMPKIN'
-#   else
-#     return 'HUH?! SPEAK UP, SONNY!'
-# ``end 
-# end 
-
 def speak_to_grandma(string)
   if string != string.upcase
     return 'HUH?! SPEAK UP, SONNY!'
